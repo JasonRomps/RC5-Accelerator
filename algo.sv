edd34abbfefe7fdeca3b7be6c63ca8392636a2e5
Jason <jasonar2@illinois.edu>
2023-09-06 14:27:40 -0500

    framework update

--- algo.sv
+++ algo.sv
@@ -3,20 +3,25 @@ module algo(
     input logic rst,
     input logic encrypt,
     input logic decrypt,
+    input logic [4:0] num_rounds,
     input logic [127:0] key,
     input logic [31:0] d_in,
-    output logic [31:0] d_out
+    output logic [31:0] d_out,
+    output logic done
 );
+
+parameter IDLE = 2'b00;
+parameter ENCRYPT_START = 2'b10;
+parameter DECRYPT_START = 2'b01;
+parameter UNDEFINED_TOP_LEVEL = 2'b11;
 
 logic [1:0] top_level_curr_state, top_level_next_state;
 
 logic encrypt_done;
 logic decrypt_done;
 
-parameter IDLE_TOP_LEVEL = 2'b00;
-parameter ENCRYPT_TOP_LEVEL = 2'b10;
-parameter DECRYPT_TOP_LEVEL = 2'b01;
-parameter UNDEFINED_TOP_LEVEL = 2'b11;
+logic [4:0] round_counter;
+
 
 
 //TOP LEVEL STATE TRANSITION
@@ -24,7 +29,7 @@ always_ff @ (posedge clk) begin
     if(~rst)
         top_level_curr_state <= IDLE;
     else
-        top_level curr_state <= top_level_next_state;
+        top_level_curr_state <= top_level_next_state;
 end
 
 //TOP LEVEL COMBO LOGIC
@@ -32,24 +37,24 @@ always_comb begin
 
     case({encrypt,decrypt})
 
-        IDLE_TOP_LEVEL: begin
+        IDLE: begin
             if(rst == 0) begin
                 d_out = 32'b0;
             end
 
         end
 
-        ENCRYPT_TOP_LEVEL: begin
+        ENCRYPT_START: begin
 
         end
 
 
-        DECRYPT_TOP_LEVEL: begin
+        DECRYPT_START: begin
 
         
         end
 
-        UNDEFINED_TOP_LEVEL: begin
+        UNDEFINED: begin
 
         
         end
