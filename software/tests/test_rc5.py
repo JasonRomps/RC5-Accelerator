a5b737bb66ab10a4a3a87181603c9171d820cc3c
Collin Meyer <collinmmeyer@gmail.com>
2023-09-06 14:27:49 -0500

    added test for rc5 runtime

--- software/tests/test_rc5.py
+++ software/tests/test_rc5.py
@@ -1,9 +1,10 @@
 import pytest
+import time
 
 from rc5 import RC5
 
 BLOCK_SIZE = 16
-NUM_ROUNDS = 15
+NUM_ROUNDS = 12
 KEY = b'\xdeadbeefdeadbeefdeadbeefdeadbeef'
 
 @pytest.fixture
@@ -15,8 +16,21 @@ def test_enc_and_dec(enc_module: RC5):
     
     data_in = b'\x00\x01\x02\03'
 
+    
     data_enc = enc_module.encryptBlock(data_in)
 
     data_dec = enc_module.decryptBlock(data_enc)
 
     assert data_dec == data_in
+
+
+def test_speed(enc_module: RC5):
+    start = time.time()
+
+    enc_module.encryptBlock(b'\x00\x01\x02\03')
+
+    end = time.time()
+
+    print(f"Encryption Run Time: {end-start}")
+
+    assert end-start < 1e-5
