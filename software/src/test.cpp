9838d4f32d76dc6efb9be663d7e022bc3409ee89
Collin Meyer <collinmmeyer@gmail.com>
2023-09-11 20:42:26 -0500

    updated testing and makefile

--- software/src/test.cpp
+++ software/src/test.cpp
@@ -1,8 +1,12 @@
 #include "catch.hpp"
 #include "rc5.h"
 
+#include <stdio.h>
+#include <iomanip>
+
+unsigned char key[BYTES] = {0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef};
+
 TEST_CASE("Single Test"){
-  unsigned char key[BYTES] = {0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef};
   rc5::word_t S[T] = {0};
 
   rc5::subkey_init(key, S);
@@ -15,15 +19,17 @@ TEST_CASE("Single Test"){
   dec = rc5::decrypt(enc, S);
 
   REQUIRE(dec == data);
+
+  printf("Simple Test Complete\n");
 }
 
 TEST_CASE("Full Cover Test"){
-  unsigned char key[BYTES] = {0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef};
   rc5::word_t S[T] = {0};
   rc5::subkey_init(key, S);
 
   uint32_t enc, dec;
-  for(rc5::block_t i = 0; i <= 0xffff; i++){
+  for(rc5::block_t i = 0; i <= 0xfffffff; i += 13){
+    printf("\rFull Test Completion (%.2f/100)", ((float)(i)/0xfffffff)*100);
     enc = rc5::encrypt(i, S);
 
     REQUIRE(enc != i);
@@ -32,4 +38,6 @@ TEST_CASE("Full Cover Test"){
 
     REQUIRE(dec == i);
   }
+
+  printf("\rFull Test Completion (100.00/100)\n");
 }
